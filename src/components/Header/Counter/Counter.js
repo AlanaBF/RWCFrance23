5f6c8ce094fb5058a245f3695f05ecd3a7399a4e
Aleksandr Riabov <sania.riabov@gmail.com>
2023-03-14 17:13:42 +0000

    Header Counter added

---
+++ src/components/Header/Counter/Counter.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Container } from 'react-bootstrap';
+
+const Counter = () => {
+  const { month, day, hour } = timeRemaining();
+
+  return (
+    <Container >
+      <div className='counter'>
+        <p className='counter-title px-2'>COUNTDOWN TO RWC 2023</p>
+        <p><span>{month}</span> Months <span>{day}</span> Days <span>{hour}</span> hours</p>
+      </div>
+    </Container>
+  )
+}
+
+export default Counter
+
+
+const timeRemaining = (endDate = '09/08/2023') => {
+  const endTime = new Date(endDate);
+  const currentTime = new Date();
+  const remaining = endTime - currentTime;
+
+  return ms(remaining)
+}
+
+
+// This function taken from: https://gist.github.com/PantheraRed/2e65c48cdfa6fba49473913300cc8b12
+// Would not work if time left is more then 1 year
+function ms(t) {
+  let year,
+    month,
+    day,
+    hour,
+    minute,
+    second;
+
+  second = Math.floor(t / 1000);
+  minute = Math.floor(second / 60);
+  second = second % 60;
+  hour = Math.floor(minute / 60);
+  minute = minute % 60;
+  day = Math.floor(hour / 24);
+  hour = hour % 24;
+  month = Math.floor(day / 30);
+  day = day % 30;
+  year = Math.floor(month / 12);
+  month = month % 12;
+
+  return { year, month, day, hour, minute, second };
+}
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,39 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Counter from './Counter/Counter.js';
 
-
-const Header = () => {
+function Header() {
   return (
-    <div>Header</div>
-  )
+    <Container fluid className='p-0'>
+      <Counter />
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">RWCFrance23</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/matches">MATCHES</Nav.Link>
+              <Nav.Link href="teams">TEAMS</Nav.Link>
+              <NavDropdown className='ms-auto' title="Tickects" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+    </Container>
+  );
 }
 
-export default Header
+export default Header;
