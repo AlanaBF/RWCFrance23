4552999fbcf0c1f1871a4710eeaeff6b46c1bf78
Ainhoa Prada <apt.code14@gmail.com>
2023-03-14 20:22:34 +0100

    adding multiple location map for venues

---
+++ src/components/Map/index.js
@@ -0,0 +1,27 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "./index.css";
+import venues from "./venues.json";
+
+function Map() {
+  console.log("here it comes");
+  return (
+    <MapContainer
+      center={[48.839778927446694, 2.337432936304376]}
+      zoom={5}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {venues.map((p) => {
+        return <Marker key={p.id} position={[p.longitude, p.latitude]}>
+          <Popup>
+            {p.name} <br /> <b>{p.city}</b>
+          </Popup>
+        </Marker>;
+      })}
+    </MapContainer>
+  );
+}
+export default Map;
