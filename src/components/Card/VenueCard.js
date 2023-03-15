9ca57e862e5153599aba32b9bde7906c3d412845
Alana Barrett-Frew <alanabarrett-frew@alanas-laptop.home>
2023-03-15 12:12:51 +0000

    Title and Venue Card components added

---
+++ src/components/Card/VenueCard.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+
+function VenueCard({ venue }) {
+    const { name, image, city, capacity, about } = venue
+
+    return (
+        <div className="card">
+            <div className="img-container">
+                <img alt={name} src={image} />
+            </div>
+            <div className="content">
+                <ul>
+                    <div>
+                        <h3><strong>Venue Name: {name}</strong></h3>
+                    </div>
+                    <div>
+                        <h3><strong>Venue City: {city}</strong></h3>
+                    </div>
+                    <div>
+                        <h4>Stadium Capacity: {capacity} </h4>
+                    </div>
+                    <div>
+                        <h5>{about}</h5>
+                    </div>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default VenueCard;
