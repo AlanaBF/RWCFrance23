c895fb7dce460eeb61f278f635e565b8990c0bfe
Alana Barrett-Frew <alanabarrett-frew@alanas-laptop.home>
2023-03-16 12:54:34 +0000

    Map fixed

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import NewsCarousel from '../../components/carousel';
 import getNews from '../../utils/api_news';
 import HeroImage from '../../components/Hero/Hero';
+import Map from "../../components/Map/index"
 const Home = () => {
   getNews()
   return (
     <div>
       <HeroImage />
       <NewsCarousel />
+      <Map />
     </div>
 
   )
