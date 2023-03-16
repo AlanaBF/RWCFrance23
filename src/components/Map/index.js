83dc2a943814283d4087d62b6cfd11b51b2a6d2e
Alana Barrett-Frew <alanabarrett-frew@alanas-laptop.home>
2023-03-16 13:01:12 +0000

    map zoom adjusted and map size reduced, testing comments deleted

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -10,8 +10,7 @@ function Map() {
   return (
     <MapContainer
       center={[48.839778927446694, 2.337432936304376]}
-      // center={[51.505, -0.09]}
-      zoom={10}
+      zoom={5}
       scrollWheelZoom={false}
     >
       <TileLayer
@@ -26,17 +25,6 @@ function Map() {
         </Marker>;
       })}
     </MapContainer>
-  //   <MapContainer className="leaflet-container" center={[51.505,-0.09]} zoom={13} scrollWheelZoom={false}>
-  //   <TileLayer
-  //     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  //     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  //   />
-  //   <Marker position={[51.505,-0.09]}>
-  //     <Popup>
-  //       A pretty CSS3 popup. <br /> Easily customizable.
-  //     </Popup>
-  //   </Marker>
-  // </MapContainer>
   );
 }
 export default Map;
