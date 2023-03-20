139634a4b8b9edfd8da4e67abb28d273b5539518
Alana Barrett-Frew <alanabarrett-frew@alanas-laptop.home>
2023-03-20 22:12:59 +0000

    Live updates complete and styled

--- src/pages/LiveMatchData/LiveMatch_Api.js
+++ src/pages/LiveMatchData/LiveMatch_Api.js
@@ -6,7 +6,7 @@ const options = {
   method: 'GET',
   url: `https://rugby-live-data.p.rapidapi.com/match/${query}`,
   headers: {
-    'X-RapidAPI-Key': '228422081bmshe889dec37a66624p1ebd42jsne3efa780d793',
+    'X-RapidAPI-Key': '651de565e1msh496d8bbe1965ca2p10954ejsne0818fbfc744',
     'X-RapidAPI-Host': 'rugby-live-data.p.rapidapi.com'
   }
 };
