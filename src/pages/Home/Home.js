9a5ffe5b716832cd3c34f13abee034ec8da40436
Ainhoa Prada <apt.code14@gmail.com>
2023-03-21 21:44:15 +0100

    chaging home background image and color

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,14 @@
 import NewsCarousel from '../../components/carousel';
 import Shop from '../../components/Shop/Shop';
 import getNews from '../../utils/api_news';
+import './index.css';
 
 import HeroImage from '../../components/Hero/Hero';
 import Map from "../../components/Map/index"
 const Home = () => {
   getNews()
   return (
-    <div>
+    <div className='home'>
       <HeroImage />
       <NewsCarousel />
       <Shop />
