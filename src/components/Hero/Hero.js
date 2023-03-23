3c368c5e3db79d342fd03dfd164c26e32f5f16a1
Aleksandr Riabov <sania.riabov@gmail.com>
2023-03-23 15:55:23 +0000

    Hero removed gap on the right on mobile

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,32 +1,28 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import hero from "../../assets/images/hero.webp"
 import './style.css'
+
 function HeroImage() {
     return (
-        <Container fluid style={{ padding: 0}}>
-            <Row>
-                <Col>
-                    <header style={{ paddingLeft: 0 }}>
+        <Container fluid style={{ padding: 0 }}>
+            <header style={{ paddingLeft: 0 }}>
+                <div
+                    className='p-5 text-center bg-image'
+                    style={{ backgroundImage: `url(${hero})` }}
+                >
+                    <div className='mask'>
                         <div
-                            className='p-5 text-center bg-image'
-                            style={{ display: 'flex', justifyContent:'center', alignItems:'center', backgroundImage: `url(${hero})`, height: 600, backgroundPosition: 'top', backgroundSize: 'cover' }}
+                            className='d-flex justify-content-center align-items-center h-100'
                         >
-                            <div className='mask'>
-                                <div className='d-flex justify-content-center align-items-center h-100'>
-                                    <div className='text-white' style={{padding:'50px 100px', backgroundColor: 'rgba(0, 0, 0, 0.6)'}}>
-                                        <h1 className='mb-3'>Rugby World Cup</h1>
-                                        <h4 className='mb-3'>France 2023</h4>
-                                        
-                                    </div>
-                                </div>
+                            <div className='hero-title' >
+                                <h1 className='mb-3'>Rugby World Cup</h1>
+                                <h4 className='mb-3'>France 2023</h4>
                             </div>
                         </div>
-                    </header>
-                </Col>
-            </Row>
+                    </div>
+                </div>
+            </header>
         </Container>
     );
 }
